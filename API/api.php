77a09a28d3a312ec8aeb3997d684877ccc679e79
Carlos Jair Lomas Fernandez <carlosjairlomasfernandez@MacBook-Pro-de-Carlos.local>
2016-08-28 18:38:56 -0500

    2

--- API/api.php
+++ API/api.php
@@ -1,7 +1,7 @@
 <?php 
 
     $output = exec('sudo pkill -9 -f /home/pi/lightshowpi/py/synchronized_lights.py');
-    $output = exec('sudo python /home/pi/lightshowpi/py/synchronized_lights.py --file=/home/pi/Music/'.$_POST["path_file"]);
+    $output = exec('sudo python /home/pi/lightshowpi/py/synchronized_lights.py --file=/var/www/html/music/'.$_POST["path_file"]);
     echo json_encode($output);
 
 
